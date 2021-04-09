11b92e05c25b9ba85fef66326182d00282cf99d4
danielburleson2 <dpburleson@crimson.ua.edu>
2021-04-08 20:03:21 -0500

    Version 0.45

--- api/Models/Transactions/ReadTransData.cs
+++ api/Models/Transactions/ReadTransData.cs
@@ -8,12 +8,45 @@ namespace api.Models.Transactions
     {
         public List<Transaction> ReadAllTrans()
         {
+             List<Transaction> allTransaction = new List<Transaction>();
 
+            ConnectionString myConnection = new ConnectionString();
+            string cs = myConnection.cs;
+
+            using var con = new MySqlConnection(cs);
+            con.Open();
+
+            string stm = "SELECT * FROM sessions ORDER BY DateOfSession DESC";
+            using var cmd = new MySqlCommand(stm, con);
+
+            using MySqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                allTransaction.Add(new Transaction(){});
+            }
+
+            return allTransactions;
         }
 
         public Transaction ReadTrans(int Id)
         {
-            
+            ConnectionString myConnection = new ConnectionString();
+            string cs = myConnection.cs;
+
+            using var con = new MySqlConnection(cs);
+            con.Open();
+
+            string stm = "SELECT * FROM sessions WHERE sessionId = @id";
+            using var cmd = new MySqlCommand(stm, con);
+            cmd.Parameters.AddWithValue("@id",Id);
+            cmd.Prepare();
+
+            using MySqlDataReader rdr = cmd.ExecuteReader();
+
+            rdr.Read();
+
+            return new Transaction(){};
         }
     }
 }
