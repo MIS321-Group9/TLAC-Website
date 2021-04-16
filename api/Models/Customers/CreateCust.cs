9e24b30bf86e15bc7e39416d0ae58b86f060e064
Zach Russell <zcrussell@crimson.ua.edu>
2021-04-15 19:51:30 -0500

    Version 0.5

--- api/Models/Customers/CreateCust.cs
+++ api/Models/Customers/CreateCust.cs
@@ -13,7 +13,7 @@ namespace api.Models.Customers
             using var con = new MySqlConnection(cs);
             con.Open();
 
-            string stm = @"CREATE TABLE tCUSTOMERS(CustomerID INT AUTO_INCREMENT NOT NULL,CustomerFName VARCHAR(20) NOT NULL,CustomerLName VARCHAR(20) NOT NULL,CustomerEmail VARCHAR(40) NOT NULL,CustomerPassword VARCHAR(20) NOT NULL,CustomerPhoneNumber INT NOT NULL,CustomerBalance DOUBLE NOT NULL,PRIMARY KEY (CustomerID),UNIQUE (CustomerEmail),UNIQUE (CustomerPhoneNumber));";
+            string stm = @"CREATE TABLE tCUSTOMERS(CustomerID INT AUTO_INCREMENT NOT NULL,CustomerFName VARCHAR(20) NOT NULL,CustomerLName VARCHAR(20) NOT NULL,CustomerEmail VARCHAR(40) NOT NULL,CustomerPassword VARCHAR(20) NOT NULL,CustomerPhoneNumber VARCHAR(11) NOT NULL,CustomerBalance DOUBLE NOT NULL,PRIMARY KEY (CustomerID),UNIQUE (CustomerEmail),UNIQUE (CustomerPhoneNumber));";
 
 
             using var cmd = new MySqlCommand(stm, con);
